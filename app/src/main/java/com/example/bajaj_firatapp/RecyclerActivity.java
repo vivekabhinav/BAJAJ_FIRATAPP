dbf6ef87070c3509d71add7535383bd068a24e09
vivek <vivekabhinav7@gmail.com>
2022-01-28 12:02:56 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/bajaj_firatapp/RecyclerActivity.java
+++ app/src/main/java/com/example/bajaj_firatapp/RecyclerActivity.java
@@ -1,10 +1,13 @@
 package com.example.bajaj_firatapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class RecyclerActivity extends AppCompatActivity {
     String[] countries = new String[]{"India","Pakistan","Dubai","Nepal","Bhutan","Egypt"};
@@ -12,6 +15,9 @@ public class RecyclerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(TAG,"creating");
+
         setContentView(R.layout.activity_recycler);
         RecyclerView countriesRecyclerView = findViewById(R.id.countriesRview); //socket
         CountriesAdapter adapter = new CountriesAdapter(countries);
@@ -19,5 +25,39 @@ public class RecyclerActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         countriesRecyclerView.setLayoutManager(layoutManager);
         countriesRecyclerView.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG,"starting");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"resuming -- restore the state of the game");
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"pausing -- save the state of the game");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,"stop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "destroy");
     }
 }
